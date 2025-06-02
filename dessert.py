e7fa9299f6e05f923dfd1bb8a6597b33d09bc3f8
CSC 101 <blmarroq@calpoly.edu>
2025-06-01 18:09:59 -0700

    commit

--- dessert.py
+++ dessert.py
@@ -11,5 +11,37 @@ class Dessert:
 
     def __eq__(self, other):
         return(self.name == other.name and
-               self.cuisine== other.cuisineself.type ===)
+               self.calories== other.calories and
+               self.flavor == other.flavor and
+               self.cuisine == other.cusine and
+               self.texture == other.texture and
+               self.type == other.type)
+
+    def __str__(self):
+        return "name: {}, calories: {}, flavor:{}, cuisine:{}, texture:{], type:{}".format(
+            self.name, self.calories, self.flavor, self.cuisine, self.texture, self.type )
+
     def __repr__(self):
+        return "name: {}, calories: {}, flavor:{}, cuisine:{}, texture:{], type:{}".format(
+            self.name, self.calories, self.flavor, self.cuisine, self.texture, self.type )
+
+    def display(self):
+        print("Name: {}".format(self.name))
+        print("Calories: {}".format(self.calories))
+        print("Flavor: {}".format(self.flavor))
+        print("Cuisine: {}".format(self.cuisine))
+        print("Type: {}".format(self.type))
+        print("Texture: {}".format(self.texture))
+
+class DessertManager:
+    def __init__(self):
+        self.desserts = []
+
+    def add_dessert(self, dessert):
+        self.desserts.append(dessert)
+
+    def find_by_flavor(self, flavor: str) -> list:
+        return [d for d in self.desserts if flavor.lower() in d.flavor.lower()]
+
+
+
---
+++ recommend.py
@@ -0,0 +1,23 @@
+# The function loads dessert data from a text file and returns a DessertManager
+# filename[str] argument is the path to the input text file containing dessert data
+# Returns a DessertManager object containing loaded Dessert instances
+def load_desserts(filename: str) -> 'DessertManager':
+    pass
+
+# The function adds a Dessert object to the DessertManager
+# dessert[Dessert] argument is a Dessert object to be added
+# Returns None
+def add_dessert(self, dessert: 'Dessert') -> None:
+    pass
+
+# The function searches desserts by flavor keyword (case-insensitive) in DessertManager
+# flavor[str] argument is the flavor keyword to search for
+# Returns a list of Dessert objects that match the flavor
+def find_by_flavor(self, flavor: str) -> list:
+    pass
+
+# The function displays the details of the Dessert object
+# No arguments
+# Returns None; prints dessert details to the console
+def display(self) -> None:
+    pass
